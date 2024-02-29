dffd4bfc7ef1b87ea35a612f25aa2f37f647a474
Pedro <pedrocribeironeto@gmail.com>
2024-02-29 20:27:19 +0000

    Teste1

--- main/main.c
+++ main/main.c
@@ -14,10 +14,10 @@ const int BTNG_PIN = 14;
 const int BTNY_PIN = 13;
 const int BTNB_PIN = 12;
 const int BTNN_PIN = 11;
-const int LEDR_PIN = 21;
-const int LEDG_PIN = 22;
-const int LEDY_PIN = 23;
-const int LEDB_PIN = 24;
+const int LEDR_PIN = 2;
+const int LEDG_PIN = 3;
+const int LEDY_PIN = 4;
+const int LEDB_PIN = 5;
 
 
 volatile int flagR = 0;
@@ -26,21 +26,166 @@ volatile int flagY = 0;
 volatile int flagB = 0;
 volatile int flagN = 0;
 
+volatile int v[] = {};
+volatile int n = 0;
+volatile int Highscore = 0;
+volatile int gameState = 0;
+volatile int round = 1;
+
+
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) {
     if (gpio == BTNR_PIN) {
       flagR = 1;
     } else if (gpio == BTNG_PIN) {
-        flagG = 2;
+        flagG = 1;
     } else if (gpio == BTNY_PIN) {
-        flagG = 3;
+        flagY = 1;
     } else if (gpio == BTNB_PIN) {
-        flagG = 4;
+        flagB = 1;
     } else if (gpio == BTNN_PIN) {
-        flagG = 5;
+        flagN = 1;
     }
   } 
 }
+
+int getRandomNumber() {
+  return rand() % 4;
+}
+
+void addNewColor() {
+    int l = getRandomNumber();
+    v[n] = l;
+    n++;
+}
+
+void piscaLED(int PIN, int freq, int duration) {
+    gpio_put(PIN, 1);
+    soundCreator(freq, duration);
+    gpio_put(PIN, 0);
+    sleep_ms(20);
+}
+
+void piscaSEQ() {
+    for (int i = 0; i < n+1; i++) {
+        if (v[i] == 0) {
+            piscaLED(LEDR_PIN, 264, 0.4);
+        } else if (v[i] == 1) {
+            piscaLED(LEDG_PIN, 330, 0.4);
+        } else if (v[i] == 2) {
+            piscaLED(LEDY_PIN, 440, 0.4);
+        } else if (v[i] == 3) {
+            piscaLED(LEDB_PIN, 528, 0.4);
+        }
+    }
+}
+
+void waitInputs() {
+    int i;
+    while ((i < n+1) && (gameState == 1)) {
+        if (flagR == 1) {
+            if (v[i] != 0) {
+                gameState = 2;
+            }
+            i++;
+            flagR = 0; 
+        } else if (flagG == 1) {
+            if (v[i] != 1) {
+                gameState = 2;
+            }
+            i++;
+            flagG = 0;
+        } else if (flagY == 1) {
+            if (v[i] != 2) {
+                gameState = 2;
+            }
+            i++;
+            flagY = 0;
+        } else if (flagB == 1) {
+            if (v[i] != 3) {
+                gameState = 2;
+            }
+            i++;
+            flagB = 0;
+        }
+        sleep_ms(30);
+    }
+    return;
+}
+
+void emptyArray() {
+    for (int i = 0; i <= n; i++) {
+        v[i] = 4;
+    }
+    if (n > Highscore) {
+        Highscore = n;
+    }
+    n = 0;
+}
+
+void soundCreator(int freq, int duration) {
+    for (int i = 0; i <= freq*duration; i++) {
+        gpio_put(BUZ_PIN, 1);
+        sleep_ms((1/freq)/2);
+        gpio_put(BUZ_PIN, 0);
+        sleep_ms((1/freq)/2);
+    }
+}
+
+void endingSong() {
+    soundCreator(264, 0.5);
+    soundCreator(198, 0.5);
+    soundCreator(165, 0.5);
+    soundCreator(132, 2);
+}
+
+void Homecoming() {
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    soundCreator(587, 0.4);
+    soundCreator(523, 0.2);
+    soundCreator(493, 1);
+    sleep_ms(400);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    sleep_ms(20);
+    soundCreator(493, 0.18);
+    soundCreator(587, 0.4);
+    soundCreator(659, 0.2);
+    soundCreator(493, 0.6);
+    soundCreator(440, 0.2);
+    soundCreator(392, 1);
+    sleep_ms(600);
+    soundCreator(392, 0.2);
+    soundCreator(369, 0.38);
+    sleep_ms(20);
+    soundCreator(369, 0.2);
+    soundCreator(392, 0.2);
+    soundCreator(369, 0.4);
+    soundCreator(293, 0.2);
+    soundCreator(329, 1);
+    soundCreator(587, 0.8);
+    soundCreator(493, 0.8);
+    soundCreator(392, 0.4);
+    soundCreator(329, 0.4);
+}
+
 
 int main() {
     stdio_init_all();
@@ -88,6 +233,24 @@ int main() {
 
 
     while (true) {
-        sleep_ms(1000);
+        Homecoming();
+        if (flagN == 1) {
+            gameState = 1; 
+            flagN = 0;       
+        }
+        while (gameState == 1) {
+            addNewColor();
+            piscaSEQ();
+            sleep_ms(500);
+            waitInputs();
+            round++;
+        } 
+        if (gameState == 2) {
+            endingSong();
+            printf("%d\n", round);
+            gameState == 0;
+            emptyArray();
+            round = 1;
+        }
     }
 }
